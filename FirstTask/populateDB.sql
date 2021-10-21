ca804c0bbaad462d5179930664cfb5dc18057119
Bohdan Holovchenko <79518893+BMHolovchenko@users.noreply.github.com>
2021-10-21 14:48:05 +0300

    Add files via upload

---
+++ FirstTask/populateDB.sql
@@ -0,0 +1,75 @@
+insert into developers values ('1','Bohdan Holovchenko','24', 'Male');
+insert into developers values ('2','Maria Mytenko','17', 'Female');
+insert into developers values ('3','William Dog','30', 'Male');
+insert into developers values ('4','Bashyr Hamster','33', 'Male');
+insert into developers values ('5','John Smith','34', 'Male');
+insert into developers values ('6','Smith Johnson','44', 'Male');
+insert into developers values ('7','Hluhivska Glafira','78', 'Female');
+
+insert into skills values ('1','Java','Junior');
+insert into skills values ('2','Java','Middle');
+insert into skills values ('3','Java','Senior');
+insert into skills values ('4','Python','Junior');
+insert into skills values ('5','Python','Middle');
+insert into skills values ('6','Python','Senior');
+insert into skills values ('7','JavaScript','Junior');
+insert into skills values ('8','JavaScript','Middle');
+insert into skills values ('9','JavaScript','Senior');
+
+insert into projects values ('1','Shop123','Shopping app');
+insert into projects values ('2','FastPost','Mail app');
+insert into projects values ('3','BestFuel','Gas station app');
+insert into projects values ('4','ManyMoney','Bank app');
+insert into projects values ('5','CleverMind','Education app');
+insert into projects values ('6','DB-Pedia','Education app');
+
+insert into companies values ('1','IT-Factory','App development for general purposes');
+insert into companies values ('2','AppDev','App development for general purposes');
+insert into companies values ('3','EducDev','App development for educational purposes');
+
+insert into customers values ('1','24/7-Banking','Bank');
+insert into customers values ('2','Best Gas Station','Chain of gas stations');
+insert into customers values ('3','European university','Educational institution');
+
+insert into developers_projects values ('1','1','1');
+insert into developers_projects values ('2','1','2');
+insert into developers_projects values ('3','2','1');
+insert into developers_projects values ('4','2','2');
+insert into developers_projects values ('5','3','3');
+insert into developers_projects values ('6','3','4');
+insert into developers_projects values ('7','4','3');
+insert into developers_projects values ('8','4','4');
+insert into developers_projects values ('9','5','5');
+insert into developers_projects values ('10','5','6');
+insert into developers_projects values ('11','6','5');
+insert into developers_projects values ('12','6','6');
+insert into developers_projects values ('13','7','1');
+insert into developers_projects values ('14','7','2');
+
+insert into developers_skills values ('1','1','3');
+insert into developers_skills values ('2','1','9');
+insert into developers_skills values ('3','2','7');
+insert into developers_skills values ('4','3','2');
+insert into developers_skills values ('5','3','5');
+insert into developers_skills values ('6','4','9');
+insert into developers_skills values ('7','4','2');
+insert into developers_skills values ('8','5','3');
+insert into developers_skills values ('9','5','8');
+insert into developers_skills values ('10','6','5');
+insert into developers_skills values ('11','6','8');
+insert into developers_skills values ('12','7','9');
+insert into developers_skills values ('13','7','6');
+
+insert into customers_projects values ('1','1','2');
+insert into customers_projects values ('2','1','4');
+insert into customers_projects values ('3','2','1');
+insert into customers_projects values ('4','2','3');
+insert into customers_projects values ('5','3','5');
+insert into customers_projects values ('6','3','6');
+
+insert into companies_projects values ('1','1','1');
+insert into companies_projects values ('2','1','3');
+insert into companies_projects values ('3','2','2');
+insert into companies_projects values ('4','2','4');
+insert into companies_projects values ('5','3','5');
+insert into companies_projects values ('6','3','6');
